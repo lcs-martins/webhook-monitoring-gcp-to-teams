e2289a65581eac4385ca3c6e4dd5ce4a1ff7e58a
Martins <lucasmartinslps@gmail.com>
2022-01-26 09:05:19 -0300

    Update teams-test.py

--- teams-test.py
+++ teams-test.py
@@ -4,7 +4,7 @@ import json
 # for local test, use http://127.0.0.1:[ PORTA ]/webhook
 # take Teams's channel webhook endpoint in 
 # [ channel ] > MENU > Connectors > search "Incoming Webhook" > do what need > take url
-webhook_url = 'https://somagrupo.webhook.office.com/webhookb2/01c2fc09-eddb-4589-b21a-2bf2651ba347@e48bd5e1-52cd-41f5-87b9-1d25c0d108b6/IncomingWebhook/085bebb9ebd74911ae11d3856ce2c199/a95b19eb-c74c-4d97-802e-6657ac288546'
+webhook_url = 'WEBHOOK ENDPOINT LINK'
 # https://docs.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-format?tabs=adaptive-md%2Cconnector-html#user-mention-in-incoming-webhook-with-adaptive-cards
 data = {
     "type": "message",
@@ -22,7 +22,7 @@ data = {
                 },
                 {
                     "type": "TextBlock",
-                    "text": "Atenção! <at>Adele AAD</at>"
+                    "text": "Attention! <at>Adele AAD</at>"
                 },
                 {
                     "type": "FactSet",
@@ -43,8 +43,8 @@ data = {
                         "type": "mention",
                         "text": "<at>Adele AAD</at>",
                         "mentioned": {
-                          "id": "lucas.lopes@somagrupo.com.br",
-                          "name": "Lucas Lopes"
+                          "id": "lucas@someone.com",
+                          "name": "Lucas Test"
                         }
                       }
                 ]
